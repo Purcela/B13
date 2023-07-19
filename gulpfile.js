c7b73ebf9db43fa7ed8bff81bc155786d0c4f0c3
Giorgi phurtseladze <99269771+Purcela@users.noreply.github.com>
2023-07-19 18:28:13 +0400

    menu

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 function buildStyles() {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('style'));
+    .pipe(gulp.dest('css/style'));
 }
 
 function watch() {
