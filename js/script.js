741cd01020978589c7880a9de2c6b53537295f64
Giorgi phurtseladze <99269771+Purcela@users.noreply.github.com>
2023-08-01 19:34:21 +0400

    +categorys+font+js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-$("#burger-menu").on("click", function(){
-    $("#menu").toggleClass("open-menu");
-  })
-$("#burger-menu1").on("click", function(){
-  $("#menu1").toggleClass("open-menu");
-})
-$("#gallery-menu").on("click", function(){
-  $("#menu2").toggleClass("open-menu");
-})
-
-
 function myFunction(imgs) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
@@ -17,28 +6,41 @@ function myFunction(imgs) {
   expandImg.parentElement.style.display = "block";
 }
 
+$('img[data-enlargable]').addClass('img-enlargable').click(function(){
+  var src = $(this).attr('src');
+  $('<div>').css({
+      background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+      backgroundSize: 'contain',
+      width:'100%', height:'100%',
+      position:'fixed',
+      zIndex:'10000',
+      top:'0', left:'0',
+      cursor: 'zoom-out'
+  }).click(function(){
+      $(this).remove();
+  }).appendTo('body');
+});
 
+// Get the gallery container and all the gallery images
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryImages = document.querySelectorAll('.gallery-img');
 
+function myFunction() {
+    var selectedCategory = document.getElementById("mySelect").value;
+
+    // Loop through all gallery images
+    galleryImages.forEach((img) => {
+        if (selectedCategory === 'all' || img.getAttribute('value') === selectedCategory.toLowerCase()) {
+            img.style.display = 'block'; // Show the image if it matches the selected category or 'All'
+        } else {
+            img.style.display = 'none'; // Hide the image if it doesn't match the selected category
+        }
+    });
+
+    document.getElementById("demo").innerHTML = "You selected: " + selectedCategory;
+}
 
 //-------------------------- TEST -------------------------------------
 
-const galleryImages = document.querySelectorAll('.gallery-img');
-const fullImgContainer = document.querySelector('.full-img');
 
-// Add click event listeners to each thumbnail image
-galleryImages.forEach((img) => {
-  img.addEventListener('click', () => {
-    // Create a new image element for the full-size image
-    const fullImg = document.createElement('img');
-    fullImg.src = img.src; // Set the source of the full-size image to the clicked thumbnail image
-    fullImg.alt = img.alt; // Set the alt text of the full-size image to the alt text of the thumbnail image
 
-    // Clear any previous content in the full-img container
-    fullImgContainer.innerHTML = '';
-
-    // Append the full-size image to the full-img container
-    fullImgContainer.appendChild(fullImg);
-  });
-});
-
-//----------------------------------------------------------------------
